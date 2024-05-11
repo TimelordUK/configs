b71cea3a12d079628e8c1e7f51b5e0504c1589da
TimelordUK <sjames8888@gmail.com>
2024-05-11 15:17:56 +0100

    change hop bindings

--- .config/nvim/lua/plugins/hop.lua
+++ .config/nvim/lua/plugins/hop.lua
@@ -1,29 +1,29 @@
 return {
-   'smoka7/hop.nvim',
-   version = "*",
-   opts = {
-      keys = 'etovxqpdygfblzhckisuran'
-   },
-   config = function() 
-      local hop = require('hop')
-      hop.setup()
-      local directions = require('hop.hint').HintDirection
+  'smoka7/hop.nvim',
+  version = "*",
+  opts = {
+    keys = 'etovxqpdygfblzhckisuran'
+  },
+  config = function() 
+    local hop = require('hop')
+    hop.setup()
 
-      vim.keymap.set('', 'f', function()
-         hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-      end, {remap=true})
-      vim.keymap.set('', 'F', function()
-         hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-      end, {remap=true})
-      vim.keymap.set('', 't', function()
-         hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-      end, {remap=true})
-      vim.keymap.set('', 'T', function()
-         hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-      end, {remap=true})
+    -- vim.keymap.set('', 'f', function()
+    --   hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+    -- end, {remap=true})
+    -- vim.keymap.set('', 'F', function()
+    --   hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+    -- end, {remap=true})
+    -- vim.keymap.set('', 't', function()
+    --   hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+    -- end, {remap=true})
+    -- vim.keymap.set('', 'T', function()
+    --   hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+    -- end, {remap=true})
 
-      vim.keymap.set('n', '<leader>hw', '<cmd>HopWord<CR>')
-      vim.keymap.set('n', '<leader>hl', '<cmd>HopLine<CR>')
-   end
+    vim.keymap.set('n', '<leader>hw', '<cmd>HopWord<CR>')
+    vim.keymap.set('n', '<leader>hl', '<cmd>HopLine<CR>')
+    vim.keymap.set('n', '<leader>ht', "<cmd>lua require'hop'.hint_char1({ direction = require('hop.hint').HintDirection.AFTER_CURSOR, current_line_only = true })<CR>")
+  end 
 }
 
