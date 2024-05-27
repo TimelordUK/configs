b09378102a5edd55dffaff0eee7086f24296fab1
TimelordUK <sjames8888@gmail.com>
2024-05-27 13:45:11 +0100

    add aerial open close shortcuts

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -1,10 +1,15 @@
 return
 {
-  'stevearc/aerial.nvim',
-  opts = {},
-  -- Optional dependencies
-  dependencies = {
-     "nvim-treesitter/nvim-treesitter",
-     "nvim-tree/nvim-web-devicons"
-  },
+   'stevearc/aerial.nvim',
+   opts = {},
+   -- Optional dependencies
+   dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
+   },
+   config = function()
+      require('aerial').setup()
+      vim.keymap.set('n', '<leader>ao', '<cmd>AerialOpen<cr>')
+      vim.keymap.set('n', '<leader>ac', '<cmd>AerialClose<cr>')
+   end
 }
