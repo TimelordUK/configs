0018b5f4abad86cb29cd428caaa14139ff2cc384
TimelordUK <sjames8888@gmail.com>
2024-06-01 11:00:16 +0100

    snippet

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -19,19 +19,21 @@ return {
 				{ "hrsh7th/nvim-cmp" },
 				{ "hrsh7th/cmp-vsnip" },
 				{ "hrsh7th/vim-vsnip" },
+				{ "SirVer/ultisnips" },
+				{ "quangnguyen30192/cmp-nvim-ultisnips" },
 			},
 			config = function()
 				local cmp = require("cmp")
-
 				cmp.setup({
 					snippet = {
 						-- REQUIRED - you must specify a snippet engine
 						expand = function(args)
 							vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+							--vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
 							-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
 							-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
 							-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-							-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+							--vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
 						end,
 					},
 					window = {
@@ -46,10 +48,10 @@ return {
 						["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 					}),
 					sources = cmp.config.sources({
+						{ name = "ultisnips" }, -- For ultisnips users.
 						{ name = "nvim_lsp" },
 						{ name = "vsnip" }, -- For vsnip users.
 						-- { name = 'luasnip' }, -- For luasnip users.
-						-- { name = 'ultisnips' }, -- For ultisnips users.
 						-- { name = 'snippy' }, -- For snippy users.
 					}, {
 						{ name = "buffer" },
@@ -205,7 +207,7 @@ return {
 			-- see for example, :help vim.lsp.buf.hover()
 
 			-- Trigger code completion
-			bufmap("i", "<C-Space>", "<C-x><C-o>")
+			-- bufmap("i", "<C-Space>", "<C-x><C-o>")
 
 			local wk = require("which-key")
 			wk.register({
