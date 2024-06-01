5a439f49a3c20596ffad9647411c7672172bf424
TimelordUK <sjames8888@gmail.com>
2024-06-01 20:29:54 +0100

    add telescope ui picker

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,17 +1,19 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.6",
+	tag = "0.1.8",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{
 			"nvim-telescope/telescope-file-browser.nvim",
 			dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 		},
+		"nvim-telescope/telescope-ui-select.nvim",
 	},
-	config = function()
+	init = function()
 		local builtin = require("telescope.builtin")
+		local telescope = require("telescope")
 		local wk = require("which-key")
-		local ext = require("telescope").extensions
+		-- ctrl-/ to see map in fb mode
 		vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
 		wk.register({
 			t = {
@@ -25,13 +27,11 @@ return {
 					end,
 					"current buffer",
 				},
-				l = { ext.file_browser.file_browser, "file browser" },
 				f = { builtin.find_files, "find files" },
 				p = { builtin.live_grep, "project grep" },
 				q = { builtin.quickfix, "quickfix" },
 				e = { builtin.buffers, "buffers" },
 				m = { builtin.man_pages, "man pages" },
-				i = { "<cmd>Telescope neoclip<CR>", "neoclip" },
 				h = { builtin.help_tags, "help tags" },
 				r = { builtin.registers, "registers" },
 				y = { builtin.filetypes, "filetypes" },
@@ -39,5 +39,15 @@ return {
 		}, {
 			prefix = "<leader>",
 		})
+		telescope.setup({
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown({
+						-- even more opts
+					}),
+				},
+			},
+		})
+		telescope.load_extension("ui-select")
 	end,
 }
