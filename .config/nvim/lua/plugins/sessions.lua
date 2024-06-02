6d43d8c64f000ed030077a203d4b575550df322d
TimelordUK <sjames8888@gmail.com>
2024-06-02 19:29:36 +0100

    change session save conflict

--- .config/nvim/lua/plugins/sessions.lua
+++ .config/nvim/lua/plugins/sessions.lua
@@ -1,22 +1,21 @@
-return
-{
-   'natecraddock/sessions.nvim',
-   config = function()
-      local sessions = require('sessions')
-      sessions.setup({
-         events = { "VimEnter" },
-         session_filepath = vim.fn.stdpath("data") .. "/sessions",
-         absolute = true,
-      })
-      sessions.stop_autosave()
+return {
+	"natecraddock/sessions.nvim",
+	config = function()
+		local sessions = require("sessions")
+		sessions.setup({
+			events = { "VimEnter" },
+			session_filepath = vim.fn.stdpath("data") .. "/sessions",
+			absolute = true,
+		})
+		sessions.stop_autosave()
 
-      _G.sessions_wrap_save = function(opts)
-         local ss = require('sessions')
-         opts = opts or {}
-         local s = vim.v.this_session
-         print(s)
-         ss.save(s)
-      end
-      vim.keymap.set("n", "<leader>ss", "<cmd>lua _G.sessions_wrap_save()<CR>") -- We can also send call options directly
-   end
+		_G.sessions_wrap_save = function(opts)
+			local ss = require("sessions")
+			opts = opts or {}
+			local s = vim.v.this_session
+			print(s)
+			ss.save(s)
+		end
+		vim.keymap.set("n", "<leader><leader>s", "<cmd>lua _G.sessions_wrap_save()<CR>") -- We can also send call options directly
+	end,
 }
