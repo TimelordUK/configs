8491928e075098c91272046204bef9fa7b016987
TimelordUK <sjames8888@gmail.com>
2024-06-02 22:48:40 +0100

    extend snippet to choose type

--- tester.py
+++ tester.py
@@ -1,15 +1,36 @@
 import unittest
 
 
+class NewClass(object):
+    """this will do it all"""
+
+
+    def myfunc(self, arg):
+
+        
+
+    def __init__(self, a1: int, a2: int, a3: str):
+        """whatever
+
+        :a1: TODO
+        :a2: TODO
+        :a3: TODO
+
+        """
+        self._a1 = a1
+        self._a2 = a2
+        self._a3 = a3
+
+
 class FileReader:
     def __init__(self) -> None:
         pass
 
     def read(self):
-        with open("/home/me/dev/py/tester.py", "r") as fh:
-            for l in fh:
-                l = l.strip()
-                print(l)
+        with open("/home/me/dev/configs/tester.py", "r") as fh:
+            for line in fh:
+                line = line.strip()
+                print(line)
 
 
 class Tester(unittest.TestCase):
