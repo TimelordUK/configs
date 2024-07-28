9e77a23fd9b9b666374f30a7f88ec2613f1f6a8b
TimelordUK <sjames8888@gmail.com>
2024-07-28 15:55:29 +0100

    fix new api for whichkey

--- .dotfiles/.config/nvim/lua/plugins/qf-helper.lua
+++ .dotfiles/.config/nvim/lua/plugins/qf-helper.lua
@@ -3,16 +3,12 @@ return {
 	config = function()
 		require("qf_helper").setup()
 		local wk = require("which-key")
-		wk.register({
-			q = {
-				name = ".qf helper", -- optional group name
-				n = { "<cmd>QNext<CR>", "next quickfix" },
-				p = { "<cmd>QPrev<CR>", "prev quickfix" },
-				q = { "<cmd>QFToggle!<CR>", "qf toggle" },
-				l = { "<cmd>LLToggle!<CR>", "ll toggle" },
-			},
-		}, {
-			prefix = "<leader>",
+		wk.add({
+			{ "<leader>q", group = ".qf helper" },
+			{ "<leader>ql", "<cmd>LLToggle!<CR>", desc = "ll toggle" },
+			{ "<leader>qn", "<cmd>QNext<CR>", desc = "next quickfix" },
+			{ "<leader>qp", "<cmd>QPrev<CR>", desc = "prev quickfix" },
+			{ "<leader>qq", "<cmd>QFToggle!<CR>", desc = "qf toggle" },
 		})
 	end,
 }
