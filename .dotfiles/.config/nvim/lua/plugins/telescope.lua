48b7875f5fe8d813ebf3dbb9f57eeceb576c587e
TimelordUK <sjames8888@gmail.com>
2024-07-28 15:30:35 +0100

    correct which key warnings on new api

--- .dotfiles/.config/nvim/lua/plugins/telescope.lua
+++ .dotfiles/.config/nvim/lua/plugins/telescope.lua
@@ -13,32 +13,27 @@ return {
 		local builtin = require("telescope.builtin")
 		local telescope = require("telescope")
 		local wk = require("which-key")
+		local bff = function()
+			builtin.current_buffer_fuzzy_find({
+				layout_strategy = "vertical",
+				layout_config = { width = 0.9, height = 0.9 },
+			})
+		end
 		-- ctrl-/ to see map in fb mode
 		vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
-		wk.register({
-			t = {
-				name = ".telescope", -- optional group name
-				b = {
-					function()
-						builtin.current_buffer_fuzzy_find({
-							layout_strategy = "vertical",
-							layout_config = { width = 0.9, height = 0.9 },
-						})
-					end,
-					"current buffer",
-				},
-				f = { builtin.find_files, "find files" },
-				p = { builtin.live_grep, "project grep" },
-				q = { builtin.quickfix, "quickfix" },
-				e = { builtin.buffers, "buffers" },
-				m = { builtin.man_pages, "man pages" },
-				h = { builtin.help_tags, "help tags" },
-				r = { builtin.registers, "registers" },
-				y = { builtin.filetypes, "filetypes" },
-			},
-		}, {
-			prefix = "<leader>",
+		wk.add({
+			{ "<leader>t", group = ".telescope" },
+			{ "<leader>tb", bff, desc = "fuzzy find" },
+			{ "<leader>te", builtin.buffers, desc = "buffers" },
+			{ "<leader>tf", builtin.find_files, desc = "find files" },
+			{ "<leader>th", builtin.help_tags, desc = "help tags" },
+			{ "<leader>tm", builtin.man_pages, desc = "man pages" },
+			{ "<leader>tp", builtin.live_grep, desc = "project grep" },
+			{ "<leader>tq", builtin.quickfix, desc = "quickfix" },
+			{ "<leader>tr", builtin.registers, desc = "registers" },
+			{ "<leader>ty", builtin.filetypes, desc = "filetypes" },
 		})
+
 		telescope.setup({
 			defaults = {
 				layout_strategy = "horizontal",
